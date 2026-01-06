3b4da5277675c42e228ab2faa27b0db69b8c4186
Siddharth Kapadne <siddharthkapadne5@gmail.com>
2026-01-06 21:42:13 +0530

    Fix nav issue

--- src/App.js
+++ src/App.js
@@ -50,11 +50,11 @@ function App() {
         <Alert
           alert={alert} />
         <Routes>
-          <Route exact path='/' element={<TextForm
+          <Route exact path='/TextUtils-React/' element={<TextForm
             heading="Enter the text"
             mode={mode}
             showAlert={showAlert} />} />
-          <Route exact path='/about' element={<About mode={mode} />} />
+          <Route exact path='/TextUtils-React/about' element={<About mode={mode} />} />
         </Routes>
       </Router>
     </>
