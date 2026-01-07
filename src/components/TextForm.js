0e4a393bc650dac4d165477676bb709411d2b670
Siddharth Kapadne <siddharthkapadne5@gmail.com>
2026-01-07 17:33:43 +0530

    Some fixes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -19,12 +19,12 @@ export default function TextForm(props) {
         setText(newText);
         props.showAlert("Text cleared!");
     }
-    
+
     const handleCopy = () => {
         navigator.clipboard.writeText(text);
         props.showAlert("Text copied to clipboard!");
     }
-    
+
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
@@ -39,17 +39,17 @@ export default function TextForm(props) {
                 <h1>{props.heading}</h1>
                 <textarea className={`${props.mode ? 'dark-mode' : 'light-mode'}`} name="text" id="text" value={text} rows="8" onChange={handleOnChange}></textarea>
                 <div className="button-container">
-                    <button className='btn' onClick={handleUpClick} >Upper Case</button>
-                    <button className='btn' onClick={handleLwClick}>Lower Case</button>
-                    <button className='btn' onClick={handleCopy}>Copy</button>
-                    <button className='btn' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
-                    <button className='btn' onClick={handleClearClick}>Clear Text</button>
+                    <button disabled={text.length === 0} className='btn' onClick={handleUpClick} >Upper Case</button>
+                    <button disabled={text.length === 0} className='btn' onClick={handleLwClick}>Lower Case</button>
+                    <button disabled={text.length === 0} className='btn' onClick={handleCopy}>Copy</button>
+                    <button disabled={text.length === 0} className='btn' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                    <button disabled={text.length === 0} className='btn' onClick={handleClearClick}>Clear Text</button>
                 </div>
             </div>
             <div className='container'>
                 <h1>Text Summary</h1>
-                <p>{text.split(" ").length} words, {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} Minutes to read</p>
+                <p>{text.split(/\s+/).filter(() => { return text.length !== 0 }).length} words, {text.length} characters</p>
+                <p>{0.008 * text.split(/\s+/).filter(() => { return text.length !== 0 }).length} Minutes to read</p>
                 <h2>Preview</h2>
                 <p className='about-text'>{text.length > 0 ? text : "Enter something in the textbox above to preview"}</p>
             </div>
